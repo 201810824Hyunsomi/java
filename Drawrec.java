510b20fcf5ad642200a3543ce70dbbbd70e7e2fe
201810824Hyunsomi <misohyun090@naver.com>
2020-08-08 10:50:58 +0900

    drawing rectangle code

---
+++ Drawrec.java
@@ -0,0 +1,33 @@
+public class myBo {
+  public static void main(String[] args){
+    Rectangle r=new Rectangle(10.0,20.0);
+    System.out.println(r.getLength());
+    Cube c=new Cube(10.0,20.0,30.0);
+    System.out.println(c.getLength());
+  }
+  class Rectangle {
+    double length;
+    double width;
+    public Rectangle(double l, double w) {
+        this.length=l;
+        this.width=w;
+    }
+    public double getLength() {
+        return length;
+    }
+    public double getWidth() {
+        return width;
+    }
+
+  }
+   class Cube extends Rectangle {
+     double height;
+     public Cube(double l, double w, double h) {
+        super(1,w);
+        this.height=h;
+     }
+     public double getHeight() {
+       return height;
+     }
+   }
+}
